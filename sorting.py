310fec07bd3687df3db4b19cb0c82bfb6628bd7a
Shao Wang <10136589+shao-wang-me@users.noreply.github.com>
2023-05-01 13:07:48 +1000

    Add Hoare's partition for quicksort

--- sorting.py
+++ sorting.py
@@ -1,10 +1,26 @@
 from random import randint
 
-def lomuto_parition(nums, l, r):
-  p = randint(l, r)  # Randomly pick a pivot
+def random_pivot(nums, l, r):
+  """Select a pivot randomly and put it to the front."""
+  p = randint(l, r)
   pivot = nums[p]
-  nums[l], nums[p] = pivot, nums[l]  # Put pivot at the front
-  n = len(nums)
+  nums[l], nums[p] = pivot, nums[l]
+  return pivot
+
+def partition_hoare(nums, l, r):
+  pivot = random_pivot(nums, l, r)
+  i, j = l + 1, r
+  while i <= j:
+    while nums[i] < pivot:
+      i += 1
+    while nums[j] >= pivot:
+      i -= 1
+    if i < j:
+      nums[i], nums[j] = nums[j], nums[i]
+  nums[l], nums[j] = nums[j], pivot
+
+def partition_lomuto(nums, l, r):
+  pivot = random_pivot(nums, l, r)
   j = l
   for i in range(l + 1, r + 1):
     if nums[i] < pivot:
@@ -22,9 +38,14 @@ def quicksort_core(nums, l, r, partition):
     quicksort_core(nums, i + 1, r, partition)
 
 def quicksort_lomuto(nums):
-  quicksort_core(nums, 0, len(nums) - 1, lomuto_parition)
+  quicksort_core(nums, 0, len(nums) - 1, partition_lomuto)
+  return nums
+
+def quicksort_hoare(nums):
+  quicksort_core(nums, 0, len(nums) - 1, partition_lomuto)
   return nums
 
 sorting_algorithms = [
-  quicksort_lomuto
+  quicksort_lomuto,
+  quicksort_hoare
 ]
