7c50d35a21308c2372d13e5413d8b67dabd855df
Shao Wang <10136589+shao-wang-me@users.noreply.github.com>
2023-05-23 19:29:19 +1000

    添加原版 Hoare repeat until 版本

--- sorting.py
+++ sorting.py
@@ -1,11 +1,13 @@
-from random import randint
+import math
+import random
 
 def random_pivot(nums, l, r):
   """Select a pivot randomly and put it to the front."""
-  p = randint(l, r)
+  p = random.randint(l, r)
   pivot = nums[p]
   nums[l], nums[p] = pivot, nums[l]
   return pivot
+  # return nums[l]
 
 def partition_hoare(nums, l, r):
   pivot = random_pivot(nums, l, r)
@@ -21,33 +23,60 @@ def partition_hoare(nums, l, r):
   nums[l], nums[j] = nums[j], pivot
   return j
 
+def partition_hoare_repeat_until(nums, l, r):  # handle all equal cases better
+  pivot = random_pivot(nums, l, r)             # because i++ and j-- when nums[i] == nums[j] == pivot
+  i, j = l, r + 1
+  while True:
+    while True:
+      i += 1
+      if nums[i] >= pivot:
+        break
+    while True:
+      j -= 1
+      if nums[j] <= pivot:
+        break
+    nums[i], nums[j] = nums[j], nums[i]
+    if i >= j:
+      break
+  nums[i], nums[j] = nums[j], nums[i]
+  nums[l], nums[j] = nums[j], nums[l]
+  return j
+
 def partition_lomuto(nums, l, r):
   pivot = random_pivot(nums, l, r)
   j = l
   for i in range(l + 1, r + 1):
     if nums[i] < pivot:
       j += 1
-      # Must have nums[j] < pivot
+      # must have nums[j] < pivot
       nums[i], nums[j] = nums[j], nums[i]
-  # Must have nums[j] < pivot
-  nums[l], nums[j] = nums[j], pivot  # Put pivot to the right place
+  # must have nums[j] < pivot
+  nums[l], nums[j] = nums[j], pivot  # put pivot to the right place
   return j
 
-def quicksort_core(nums, l, r, partition):
+def quicksort_core(nums, l, r, partition) -> int:  # returns the number of iterations
   if l < r:
     i = partition(nums, l, r)
-    quicksort_core(nums, l, i - 1, partition)
-    quicksort_core(nums, i + 1, r, partition)
+    left_iterations = quicksort_core(nums, l, i - 1, partition)
+    right_iterations = quicksort_core(nums, i + 1, r, partition)
+    return max(left_iterations, right_iterations) + 1
+  return 0
 
 def quicksort_lomuto(nums):
-  quicksort_core(nums, 0, len(nums) - 1, partition_lomuto)
-  return nums
+  iterations = quicksort_core(nums, 0, len(nums) - 1, partition_lomuto)
+  return nums, iterations
 
 def quicksort_hoare(nums):
-  quicksort_core(nums, 0, len(nums) - 1, partition_hoare)
-  return nums
+  iterations = quicksort_core(nums, 0, len(nums) - 1, partition_hoare)
+  return nums, iterations
+
+def quicksort_hoare_repeat_until(nums):
+  nums.append(math.inf)  # append a sentinel to prevent index i from advancing beyond position n
+  iterations = quicksort_core(nums, 0, len(nums) - 2, partition_hoare_repeat_until)  # thus r = n - 2 here
+  return nums[:-1], iterations  # and remove the last math.inf
 
 sorting_algorithms = [
   quicksort_lomuto,
-  quicksort_hoare
+  quicksort_hoare,
+  quicksort_hoare_repeat_until
 ]
--- test_sorting.py
+++ test_sorting.py
@@ -7,21 +7,31 @@ class TestSorting(TestCase):
     self.cases = [
       [],
       [1],
-      [4,2,1,6,8,3,1,3,7,9],
       [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+      [4,2,1,6,8,3,1,3,7,9],
       [1,2,3,4,5,6,7,8,9,10,11,12,13],
       [-4,2,4,1,2,5,8,9,-12,28192,39,6.7],
       [5,4,3,2,1]
     ]
 
-    for _ in range(100):
-      self.cases.append([random() - 0.5 for _ in range(100)])
+    for _ in range(1000):
+      self.cases.append([random() - 0.5 for _ in range(1000)])  # random cases
+      self.cases.append([1 for _ in range(100)])  # all equal cases
+
+    print('generated test cases')
 
   def test_sorting(self):
+    iteration_stats = {}
     for sort in sorting_algorithms:
-      with self.subTest(sorting_algorith=sort.__name__):
+      sorting_algorithm_name = sort.__name__
+      iteration_stats[sorting_algorithm_name] = 0
+      with self.subTest(sorting_algorith=sorting_algorithm_name):
         for nums in self.cases:
-          self.assertEqual(sort(nums), sorted(nums))
+          # make copies of nums, in case the elements are accidentally modified
+          sorted_nums, iterations = sort(nums[:])
+          self.assertEqual(sorted_nums, sorted(nums[:]))
+          iteration_stats[sorting_algorithm_name] += iterations
+    print(iteration_stats)
 
 if __name__ == '__main__':
   unittest.main()
